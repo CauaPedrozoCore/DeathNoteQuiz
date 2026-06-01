6ffdc1997f7f7ae47a3ed127fad1e76c8059dcf5
Gabriel Oliveira Silva <gabriel.osilvaccor@gmail.com>
2026-06-01 15:22:50 -0300

    Add files via upload

---
+++ js/bdQuiz.js
@@ -0,0 +1,50 @@
+const perguntas = [
+    {
+        pergunta: "Quantos segundos uma pessoa vive apos ter seu nome escrito no Death Note?",
+        imagem: "BancoImgs/questaoUm.jpeg",
+        alternativas: ["10 Segundos", "40 Segundos", "50 Segundos", "60 Segundos"],
+        correta: 1
+    },
+    {
+        pergunta: "Qual e o nome verdadeiro de L?",
+        imagem: "BancoImgs/questao2.jpeg",
+        alternativas: ["Ryuzaki", "L Lawliet", "Lind L. Taylor", "L não tem nome verdadeiro confirmado"],
+        correta: 1
+    },
+    {
+        pergunta: "O que acontece se voce escrever um nome errado no Death Note 4 vezes?",
+        imagem: "BancoImgs/questao3.jpeg",
+        alternativas: ["Você Morre", "O Caderno para de funcionar", "Nada Acontece", "A Pessoa ganha imunidade"],
+        correta: 2
+    },
+    {
+        pergunta: "Quem foi o Terceiro Kira?",
+        imagem: "BancoImgs/questao4.jpeg",
+        alternativas: ["Kyosuke Higuchi", "Kiyomi Takada", "Misa Amane", "Teru Mikami"],
+        correta: 0
+    },
+    {
+        pergunta: "E o que ele era?",
+        imagem: "BancoImgs/questao5.jpeg",
+        alternativas: ["Um Humano com muito senso de justiça", "Uma Repórter da TV Sakura", "Um Empresário Ganancioso", "Um Assassino que já matava antes da posse do Death Note"],
+        correta: 2
+    },
+    {
+        pergunta: "Quem matou o L?",
+        imagem: "BancoImgs/questao6.jpeg",
+        alternativas: ["Light Yagami", "Misa Amane", "Shinigami Rem", "Shinigami Ryuk"],
+        correta: 2
+    },
+    {
+        pergunta: "Quem e a personagem na foto?",
+        imagem: "BancoImgs/questao7.jpeg",
+        alternativas: ["Ryome Takada", "Misa Amane", "Mello", "Nenhum desses!"],
+        correta: 1
+    },
+    {
+        pergunta: "TERU MIKAMI----",
+        imagem: "BancoImgs/questao8.jpg", // Note que este arquivo está como .jpg na árvore de arquivos
+        alternativas: ["Quarto Kira", "Escolhido por Light Yagami", "Passou a vida defendendo os Fracos", "DELETAR DELETAR DELETAR DELETAR"],
+        correta: 3
+    }
+];
